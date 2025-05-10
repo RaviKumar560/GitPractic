d58d3034a180ef2e09e20c2b9af73162a4242ec4
RaviKumar560 <rk5118754@gmail.com>
2025-05-10 15:52:40 +0530

    Update greateController.java

--- src/main/java/in/sp/main/Controller/greateController.java
+++ src/main/java/in/sp/main/Controller/greateController.java
@@ -1,1 +1,22 @@
+package in.sp.main.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllers {
+	
+	@GetMapping("/greet")
+	public String Display() {
+
+
+		String s2="good Morning";
+		String s3="Mam";
+	
+		
+
+		return s1+" "+s2+" "+s3;
+
+
+		
+	}
